7cbe7fb190b001c7dea4cf5bd2b437fdd34cddb7
Eric Torreborre <etorreborre@yahoo.com>
2017-11-27 21:49:35 +0100

    fixed the compilation of routes

--- core/src/main/scala/org/http4s/rho/bits/ResultMatcher.scala
+++ core/src/main/scala/org/http4s/rho/bits/ResultMatcher.scala
@@ -6,36 +6,46 @@ import org.http4s.rho.Result
 
 import scala.reflect.runtime.universe.{Type, WeakTypeTag}
 
-trait ResultMatcher[F[_], R] {
+trait ResultMatcher[F[_], -R] {
   def encodings: Set[MediaType]
   def resultInfo: Set[ResultInfo]
 
   def conv(req: Request[F], r: R)(implicit F: Monad[F]): F[Response[F]]
 }
 
-object ResultMatcher {
+object ResultMatcher extends ResultMatcher1 {
 
   sealed trait MaybeWritable[T] {
     def contentType: Set[MediaType]
+
     def resultInfo: Option[Type]
+
     def encodings: Set[MediaType]
   }
 
-  object MaybeWritable {
+  object MaybeWritable extends MaybeWritable1 {
 
     // This will represent a "missing" result meaning this status wasn't used
     implicit val maybeWritableAny: MaybeWritable[Any] = new MaybeWritable[Any] {
       override def contentType: Set[MediaType] = Set.empty
+
       override def encodings: Set[MediaType] = Set.empty
+
       override def resultInfo: Option[Type] = None
     }
+  }
+
+  trait MaybeWritable1 {
 
     /* Allowing the `Writable` to be `null` only matches real results but allows for
        situations where you return the same status with two types */
     implicit def maybeIsWritable[F[_], T](implicit t: WeakTypeTag[T], w: EntityEncoder[F, T] = null): MaybeWritable[T] = new MaybeWritable[T] {
       private val ww = Option(w)
+
       override def contentType: Set[MediaType] = ww.flatMap(_.contentType.map(_.mediaType)).toSet
+
       override def encodings: Set[MediaType] = ww.flatMap(_.contentType.map(_.mediaType)).toSet
+
       override def resultInfo: Option[Type] = Some(t.tpe.dealias)
     }
   }
@@ -221,73 +231,7 @@ object ResultMatcher {
     LOOPDETECTED,
     NOTEXTENDED,
     NETWORKAUTHENTICATIONREQUIRED]] =
-  new ResultMatcher[F, Result[
-    F,
-    CONTINUE,
-    SWITCHINGPROTOCOLS,
-    PROCESSING,
-
-    OK,
-    CREATED,
-    ACCEPTED,
-    NONAUTHORITATIVEINFORMATION,
-    NOCONTENT,
-    RESETCONTENT,
-    PARTIALCONTENT,
-    MULTISTATUS,
-    ALREADYREPORTED,
-    IMUSED,
-
-    MULTIPLECHOICES,
-    MOVEDPERMANENTLY,
-    FOUND,
-    SEEOTHER,
-    NOTMODIFIED,
-    USEPROXY,
-    TEMPORARYREDIRECT,
-    PERMANENTREDIRECT,
-
-    BADREQUEST,
-    UNAUTHORIZED,
-    PAYMENTREQUIRED,
-    FORBIDDEN,
-    NOTFOUND,
-    METHODNOTALLOWED,
-    NOTACCEPTABLE,
-    PROXYAUTHENTICATIONREQUIRED,
-    REQUESTTIMEOUT,
-    CONFLICT,
-    GONE,
-    LENGTHREQUIRED,
-    PRECONDITIONFAILED,
-    PAYLOADTOOLARGE,
-    URITOOLONG,
-    UNSUPPORTEDMEDIATYPE,
-    RANGENOTSATISFIABLE,
-    EXPECTATIONFAILED,
-    UNPROCESSABLEENTITY,
-    LOCKED,
-    FAILEDDEPENDENCY,
-    UPGRADEREQUIRED,
-    PRECONDITIONREQUIRED,
-    TOOMANYREQUESTS,
-    REQUESTHEADERFIELDSTOOLARGE,
-
-    INTERNALSERVERERROR,
-    NOTIMPLEMENTED,
-    BADGATEWAY,
-    SERVICEUNAVAILABLE,
-    GATEWAYTIMEOUT,
-    HTTPVERSIONNOTSUPPORTED,
-    VARIANTALSONEGOTIATES,
-    INSUFFICIENTSTORAGE,
-    LOOPDETECTED,
-    NOTEXTENDED,
-    NETWORKAUTHENTICATIONREQUIRED]] {
-    override lazy val encodings: Set[MediaType] =
-      allTpes.flatMap { case (_, m) => m.encodings }.toSet
-
-    override def conv(req: Request[F], r: Result[
+    new ResultMatcher[F, Result[
       F,
       CONTINUE,
       SWITCHINGPROTOCOLS,
@@ -349,101 +293,161 @@ object ResultMatcher {
       INSUFFICIENTSTORAGE,
       LOOPDETECTED,
       NOTEXTENDED,
-      NETWORKAUTHENTICATIONREQUIRED])(implicit F: Monad[F]): F[Response[F]] = F.pure(r.resp)
+      NETWORKAUTHENTICATIONREQUIRED]] {
+      override lazy val encodings: Set[MediaType] =
+        allTpes.flatMap { case (_, m) => m.encodings }.toSet
 
-    override def resultInfo: Set[ResultInfo] = {
-      allTpes.flatMap { case (s, mw) =>
-        mw.resultInfo.map( t => StatusAndType(s, t))
-      }.toSet
-    }
+      override def conv(req: Request[F], r: Result[
+        F,
+        CONTINUE,
+        SWITCHINGPROTOCOLS,
+        PROCESSING,
 
-    private lazy val allTpes: List[(Status, MaybeWritable[_])] = {
-      import Status._
-      List(
-        (Continue, mCONTINUE),
-        (SwitchingProtocols, mSWITCHINGPROTOCOLS),
-        (Processing, mPROCESSING),
-        (Ok, mOK),
-        (Created, mCREATED),
-        (Accepted, mACCEPTED),
-        (NonAuthoritativeInformation, mNONAUTHORITATIVEINFORMATION),
-        (NoContent, mNOCONTENT),
-        (ResetContent, mRESETCONTENT),
-        (PartialContent, mPARTIALCONTENT),
-        (MultiStatus, mMULTISTATUS),
-        (AlreadyReported, mALREADYREPORTED),
-        (IMUsed, mIMUSED),
-        (MultipleChoices, mMULTIPLECHOICES),
-        (MovedPermanently, mMOVEDPERMANENTLY),
-        (Found, mFOUND),
-        (SeeOther, mSEEOTHER),
-        (NotModified, mNOTMODIFIED),
-        (UseProxy, mUSEPROXY),
-        (TemporaryRedirect, mTEMPORARYREDIRECT),
-        (PermanentRedirect, mPERMANENTREDIRECT),
-        (BadRequest, mBADREQUEST),
-        (Unauthorized, mUNAUTHORIZED),
-        (PaymentRequired, mPAYMENTREQUIRED),
-        (Forbidden, mFORBIDDEN),
-        (NotFound, mNOTFOUND),
-        (MethodNotAllowed, mMETHODNOTALLOWED),
-        (NotAcceptable, mNOTACCEPTABLE),
-        (ProxyAuthenticationRequired, mPROXYAUTHENTICATIONREQUIRED),
-        (RequestTimeout, mREQUESTTIMEOUT),
-        (Conflict, mCONFLICT),
-        (Gone, mGONE),
-        (LengthRequired, mLENGTHREQUIRED),
-        (PreconditionFailed, mPRECONDITIONFAILED),
-        (PayloadTooLarge, mPAYLOADTOOLARGE),
-        (UriTooLong, mURITOOLONG),
-        (UnsupportedMediaType, mUNSUPPORTEDMEDIATYPE),
-        (RangeNotSatisfiable, mRANGENOTSATISFIABLE),
-        (ExpectationFailed, mEXPECTATIONFAILED),
-        (UnprocessableEntity, mUNPROCESSABLEENTITY),
-        (Locked, mLOCKED),
-        (FailedDependency, mFAILEDDEPENDENCY),
-        (UpgradeRequired, mUPGRADEREQUIRED),
-        (PreconditionRequired, mPRECONDITIONREQUIRED),
-        (TooManyRequests, mTOOMANYREQUESTS),
-        (RequestHeaderFieldsTooLarge, mREQUESTHEADERFIELDSTOOLARGE),
-        (InternalServerError, mINTERNALSERVERERROR),
-        (NotImplemented, mNOTIMPLEMENTED),
-        (BadGateway, mBADGATEWAY),
-        (ServiceUnavailable, mSERVICEUNAVAILABLE),
-        (GatewayTimeout, mGATEWAYTIMEOUT),
-        (HttpVersionNotSupported, mHTTPVERSIONNOTSUPPORTED),
-        (VariantAlsoNegotiates, mVARIANTALSONEGOTIATES),
-        (InsufficientStorage, mINSUFFICIENTSTORAGE),
-        (LoopDetected, mLOOPDETECTED),
-        (NotExtended, mNOTEXTENDED),
-        (NetworkAuthenticationRequired, mNETWORKAUTHENTICATIONREQUIRED)
-      )
+        OK,
+        CREATED,
+        ACCEPTED,
+        NONAUTHORITATIVEINFORMATION,
+        NOCONTENT,
+        RESETCONTENT,
+        PARTIALCONTENT,
+        MULTISTATUS,
+        ALREADYREPORTED,
+        IMUSED,
+
+        MULTIPLECHOICES,
+        MOVEDPERMANENTLY,
+        FOUND,
+        SEEOTHER,
+        NOTMODIFIED,
+        USEPROXY,
+        TEMPORARYREDIRECT,
+        PERMANENTREDIRECT,
+
+        BADREQUEST,
+        UNAUTHORIZED,
+        PAYMENTREQUIRED,
+        FORBIDDEN,
+        NOTFOUND,
+        METHODNOTALLOWED,
+        NOTACCEPTABLE,
+        PROXYAUTHENTICATIONREQUIRED,
+        REQUESTTIMEOUT,
+        CONFLICT,
+        GONE,
+        LENGTHREQUIRED,
+        PRECONDITIONFAILED,
+        PAYLOADTOOLARGE,
+        URITOOLONG,
+        UNSUPPORTEDMEDIATYPE,
+        RANGENOTSATISFIABLE,
+        EXPECTATIONFAILED,
+        UNPROCESSABLEENTITY,
+        LOCKED,
+        FAILEDDEPENDENCY,
+        UPGRADEREQUIRED,
+        PRECONDITIONREQUIRED,
+        TOOMANYREQUESTS,
+        REQUESTHEADERFIELDSTOOLARGE,
+
+        INTERNALSERVERERROR,
+        NOTIMPLEMENTED,
+        BADGATEWAY,
+        SERVICEUNAVAILABLE,
+        GATEWAYTIMEOUT,
+        HTTPVERSIONNOTSUPPORTED,
+        VARIANTALSONEGOTIATES,
+        INSUFFICIENTSTORAGE,
+        LOOPDETECTED,
+        NOTEXTENDED,
+        NETWORKAUTHENTICATIONREQUIRED])(implicit F: Monad[F]): F[Response[F]] = F.pure(r.resp)
+
+      override def resultInfo: Set[ResultInfo] = {
+        allTpes.flatMap { case (s, mw) =>
+          mw.resultInfo.map(t => StatusAndType(s, t))
+        }.toSet
+      }
+
+      private lazy val allTpes: List[(Status, MaybeWritable[_])] = {
+        import Status._
+        List(
+          (Continue, mCONTINUE),
+          (SwitchingProtocols, mSWITCHINGPROTOCOLS),
+          (Processing, mPROCESSING),
+          (Ok, mOK),
+          (Created, mCREATED),
+          (Accepted, mACCEPTED),
+          (NonAuthoritativeInformation, mNONAUTHORITATIVEINFORMATION),
+          (NoContent, mNOCONTENT),
+          (ResetContent, mRESETCONTENT),
+          (PartialContent, mPARTIALCONTENT),
+          (MultiStatus, mMULTISTATUS),
+          (AlreadyReported, mALREADYREPORTED),
+          (IMUsed, mIMUSED),
+          (MultipleChoices, mMULTIPLECHOICES),
+          (MovedPermanently, mMOVEDPERMANENTLY),
+          (Found, mFOUND),
+          (SeeOther, mSEEOTHER),
+          (NotModified, mNOTMODIFIED),
+          (UseProxy, mUSEPROXY),
+          (TemporaryRedirect, mTEMPORARYREDIRECT),
+          (PermanentRedirect, mPERMANENTREDIRECT),
+          (BadRequest, mBADREQUEST),
+          (Unauthorized, mUNAUTHORIZED),
+          (PaymentRequired, mPAYMENTREQUIRED),
+          (Forbidden, mFORBIDDEN),
+          (NotFound, mNOTFOUND),
+          (MethodNotAllowed, mMETHODNOTALLOWED),
+          (NotAcceptable, mNOTACCEPTABLE),
+          (ProxyAuthenticationRequired, mPROXYAUTHENTICATIONREQUIRED),
+          (RequestTimeout, mREQUESTTIMEOUT),
+          (Conflict, mCONFLICT),
+          (Gone, mGONE),
+          (LengthRequired, mLENGTHREQUIRED),
+          (PreconditionFailed, mPRECONDITIONFAILED),
+          (PayloadTooLarge, mPAYLOADTOOLARGE),
+          (UriTooLong, mURITOOLONG),
+          (UnsupportedMediaType, mUNSUPPORTEDMEDIATYPE),
+          (RangeNotSatisfiable, mRANGENOTSATISFIABLE),
+          (ExpectationFailed, mEXPECTATIONFAILED),
+          (UnprocessableEntity, mUNPROCESSABLEENTITY),
+          (Locked, mLOCKED),
+          (FailedDependency, mFAILEDDEPENDENCY),
+          (UpgradeRequired, mUPGRADEREQUIRED),
+          (PreconditionRequired, mPRECONDITIONREQUIRED),
+          (TooManyRequests, mTOOMANYREQUESTS),
+          (RequestHeaderFieldsTooLarge, mREQUESTHEADERFIELDSTOOLARGE),
+          (InternalServerError, mINTERNALSERVERERROR),
+          (NotImplemented, mNOTIMPLEMENTED),
+          (BadGateway, mBADGATEWAY),
+          (ServiceUnavailable, mSERVICEUNAVAILABLE),
+          (GatewayTimeout, mGATEWAYTIMEOUT),
+          (HttpVersionNotSupported, mHTTPVERSIONNOTSUPPORTED),
+          (VariantAlsoNegotiates, mVARIANTALSONEGOTIATES),
+          (InsufficientStorage, mINSUFFICIENTSTORAGE),
+          (LoopDetected, mLOOPDETECTED),
+          (NotExtended, mNOTEXTENDED),
+          (NetworkAuthenticationRequired, mNETWORKAUTHENTICATIONREQUIRED)
+        )
+      }
     }
-  }
 
   implicit def optionMatcher[F[_], R](implicit o: WeakTypeTag[R], w: EntityEncoder[F, R]): ResultMatcher[F, Option[R]] = new ResultMatcher[F, Option[R]] {
     override val encodings: Set[MediaType] = w.contentType.map(_.mediaType).toSet
     override val resultInfo: Set[ResultInfo] = Set(StatusAndType(Status.Ok, o.tpe.dealias),
-                                                   StatusOnly(Status.NotFound))
+      StatusOnly(Status.NotFound))
 
     override def conv(req: Request[F], r: Option[R])(implicit F: Monad[F]): F[Response[F]] = r match {
       case Some(res) => ResponseGeneratorInstances.Ok[F].pure(res)
-      case None      => ResponseGeneratorInstances.NotFound[F].pure(req.uri.path)
+      case None => ResponseGeneratorInstances.NotFound[F].pure(req.uri.path)
     }
   }
 
   implicit def writableMatcher[F[_], R](implicit o: WeakTypeTag[R], w: EntityEncoder[F, R]): ResultMatcher[F, R] = new ResultMatcher[F, R] {
     override def encodings: Set[MediaType] = w.contentType.map(_.mediaType).toSet
+
     override def resultInfo: Set[ResultInfo] = Set(StatusAndType(Status.Ok, o.tpe.dealias))
 
     override def conv(req: Request[F], r: R)(implicit F: Monad[F]): F[Response[F]] = ResponseGeneratorInstances.Ok[F].pure(r)
-  }
-
-  implicit def fMatcher[F[_], R](implicit r: ResultMatcher[F, R], w: EntityEncoder[F, R]): ResultMatcher[F, F[R]] = new ResultMatcher[F, F[R]] {
-    override def encodings: Set[MediaType] = r.encodings
-    override def resultInfo: Set[ResultInfo] = r.resultInfo
-
-    override def conv(req: Request[F], f: F[R])(implicit F: Monad[F]): F[Response[F]] = F.flatMap(f)(r.conv(req, _))
   }
 
   implicit def responseMatcher[F[_]]: ResultMatcher[F, Response[F]] = new ResultMatcher[F, Response[F]] {
@@ -451,5 +455,15 @@ object ResultMatcher {
     override def resultInfo: Set[ResultInfo] = Set.empty
 
     override def conv(req: Request[F], r: Response[F])(implicit F: Monad[F]): F[Response[F]] = F.pure(r)
+  }
+}
+
+trait ResultMatcher1 {
+  implicit def fMatcher[F[_], R](implicit r: ResultMatcher[F, R]): ResultMatcher[F, F[R]] = new ResultMatcher[F, F[R]] {
+    override def encodings: Set[MediaType] = r.encodings
+
+    override def resultInfo: Set[ResultInfo] = r.resultInfo
+
+    override def conv(req: Request[F], f: F[R])(implicit F: Monad[F]): F[Response[F]] = F.flatMap(f)(r.conv(req, _))
   }
 }
